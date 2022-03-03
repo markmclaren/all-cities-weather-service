99b31fd888bf1fddbbf11b2438be9e0d7079859f
markmclaren <mark.mclaren@gmail.com>
2022-03-03 15:09:54 +0000

    Use Lombok

--- src/main/java/com/example/demo/Weather.java
+++ src/main/java/com/example/demo/Weather.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import lombok.Data;
+
+@Data
 public class Weather {
 
     private String city;
@@ -8,27 +11,4 @@ public class Weather {
 
     private String icon;
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
 }
